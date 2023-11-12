18c200e7f861f2c7de7713768c26581951651d44
anilklc <hage4950@gmail.com>
2023-11-13 00:23:38 +0300

    Performance control add

--- Core/Utilities/Interceptors/AspectInterceptorSelector.cs
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -1,4 +1,5 @@
 ﻿using Castle.DynamicProxy;
+using Core.Aspects.AutoFac.Performance;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using System;
 using System.Collections.Generic;
@@ -18,8 +19,9 @@ namespace Core.Utilities.Interceptors
             var methodAttributes = type.GetMethod(method.Name)
                 .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
             classAttributes.AddRange(methodAttributes);
-           // classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger)));  logger eklemek istersek
-            
+            // classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger)));  logger eklemek istersek
+            classAttributes.Add(new PerformanceAspect(0));
+
 
             return classAttributes.OrderBy(x => x.Priority).ToArray();
         }
